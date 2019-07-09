a0ba3b33f39e4ace2111101b9194ae8baed682da
OliverForeman <oliverforeman@btinternet.com>
2019-07-09 11:29:09 +0100

    SDE-23 Created Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+const Header = ({ email }) => (
+  <header className="container-header">
+    <ul role="navigation">
+      <li className="title-header">Helixer Search</li>
+      <li className="email-header">{email}</li>
+    </ul>
+  </header>
+);
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired
+};
+
+export default Header;
