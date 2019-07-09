32a05ae24b03f1fa6b107d1b6c0e026ed51fccd4
OliverForeman <oliverforeman@btinternet.com>
2019-07-09 13:32:56 +0100

    SDE-23 Added mockup of pagination display

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
 import Header from '../header/Header';
+import ReactPaginate from 'react-paginate';
 
-const App = () => (
-  <div className="app">
-    <Header email="oforeman@scottlogic.com" />
-    <div className="paper-display-page">
-      <div>Search functionality goes here</div>
-      <div>Search results</div>
-      <div>Pagination controls</div>
+const App = () => {
+  const handlePageChange = ({ selected }) => {
+    console.log(selected);
+  };
+
+  return (
+    <div className="app">
+      <Header email="oforeman@scottlogic.com" />
+      <div style={{ display: 'flex', flexDirection: 'column' }} className="paper-display-page">
+        <div style={{ margin: 'auto' }}>Search functionality goes here</div>
+        <div style={{ margin: 'auto' }}>Search results</div>
+        <ReactPaginate
+          pageCount={50}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
