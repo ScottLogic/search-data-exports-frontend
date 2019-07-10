76ec9ae1045ef9e92e2821a64dc39617afe90bf4
OliverForeman <oliverforeman@btinternet.com>
2019-07-10 09:49:42 +0100

    SDE-23 Updated app and created custom hook

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,18 +2,42 @@ import React from 'react';
 import './App.css';
 import Header from '../header/Header';
 import ReactPaginate from 'react-paginate';
+import { useInputForm } from '../../utilities/hooks';
+import ResultList from '../list/ResultList';
 
 const App = () => {
+  const searchCriteria = useInputForm('');
+
   const handlePageChange = ({ selected }) => {
     console.log(selected);
+  };
+
+  const handleSearch = e => {
+    e.preventDefault();
   };
 
   return (
     <div className="app">
       <Header email="oforeman@scottlogic.com" />
-      <div style={{ display: 'flex', flexDirection: 'column' }} className="paper-display-page">
-        <div style={{ margin: 'auto' }}>Search functionality goes here</div>
-        <div style={{ margin: 'auto' }}>Search results</div>
+      <div style={{ flexDirection: 'column' }} className="paper-display-page">
+        <div style={{ margin: 'auto', padding: '1vmin' }}>
+          <form onSubmit={handleSearch}>
+            <div>
+              <input type="radio" id="userPostSearch" name="indexSearch" value="both" defaultChecked />
+              <label htmlFor="userPostSearch">Both</label>
+              <input type="radio" id="userSearch" name="indexSearch" value="user" />
+              <label htmlFor="userSearch">User</label>
+              <input type="radio" id="postSearch" name="indexSearch" value="post" />
+              <label htmlFor="postSearch">Post</label>
+            </div>
+            <input name="searchInput" placeholder="Input search criteria" type="text" {...searchCriteria} required />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <div style={{ margin: 'auto', flexGrow: 2 }}>
+          <ResultList data={['Result 1', 'Result 2']} />    
+        </div>
+
         <ReactPaginate
           pageCount={50}
           pageRangeDisplayed={5}
