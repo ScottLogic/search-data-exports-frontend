a9c7c4126cc803636f708c46ca2a3ec7bca24ca7
OliverForeman <oliverforeman@btinternet.com>
2019-07-12 13:52:22 +0100

    SDE-29 Added tests for request creation and updates

---
+++ src/utilities/requestCreator.test.js
@@ -0,0 +1,47 @@
+import { createRequest, updateRequestPage } from './requestCreator';
+
+describe('Request Creator', () => {
+  const type = 'user';
+  const results = 10;
+  const page = 0;
+  const searchVal = 'Test Search'
+  
+  it('createRequest returns an API request object', () => {
+    const expectedRequest = {
+      type,
+      results,
+      page,
+      search: [
+        { field: 'all', value: searchVal }
+      ]
+    };
+
+    const request = createRequest(type, results, page, searchVal);
+
+    expect(request).toEqual(expectedRequest);
+  });
+
+  it('updateRequestPage returns the given request with the updated page', () => {
+    const newPage = 1;
+    const initialRequest = {
+      type,
+      results,
+      page,
+      search: [
+        { field: 'all', value: searchVal }
+      ]
+    };
+    const expectedRequest = {
+      type,
+      results,
+      page: newPage,
+      search: [
+        { field: 'all', value: searchVal }
+      ]
+    };
+
+    const request = updateRequestPage(initialRequest, newPage);
+
+    expect(request).toEqual(expectedRequest);
+  });
+});
