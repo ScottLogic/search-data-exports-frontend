0beb72d8681d5931b8022014bbdf7e66e9fcdb18
Antreas Paschalis <apaschalis@scottlogic.com>
2019-07-17 11:39:09 +0100

    SDE-6 Added export results button

--- src/components/list/ResultList.jsx
+++ src/components/list/ResultList.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './ResultList.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./ResultList.css";
 
-const ResultList = ({ data }) => (
-  <ul className="result-list">
-    {data.map((value, index) => <li key={index}>{resultFormat(value._source)}</li>)}
-  </ul>
-);
+const ResultList = ({ data, handleExportClick }) => {
+  const exportButton = data.length ? (<button onClick={handleExportClick}>Export Results</button>) : ("");
+  return (
+    <ul className="result-list">
+      {exportButton}
+      {data.map((value, index) => (
+        <li key={index}>{resultFormat(value._source)}</li>
+      ))}
+    </ul>
+  );
+};
 
 const resultFormat = ({ UserID, DateCreated, Content, Tags }) => (
   <div className="container-result-row">
@@ -19,15 +25,14 @@ const resultFormat = ({ UserID, DateCreated, Content, Tags }) => (
       {Content}
     </div>
     <div className="container-result-row-contents container-result-row-tags">
-      <i>
-        {Tags.map(tag => `${tag} `)}
-      </i>
+      <i>{Tags.map(tag => `${tag} `)}</i>
     </div>
   </div>
 );
 
 ResultList.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object).isRequired
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleExportClick: PropTypes.func.isRequired
 };
 
 export default ResultList;
