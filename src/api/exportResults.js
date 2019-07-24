3e5486e4f2f106c6a1c13bf6b1d324d233d0f25c
OliverForeman <oliverforeman@btinternet.com>
2019-07-24 15:27:58 +0100

    SDE-76 Updated export results api calls

--- src/api/exportResults.js
+++ src/api/exportResults.js
@@ -1,26 +1,49 @@
+import { toast } from 'react-toastify';
+
 const getDownloadRequest = (type, parameters, searchCriteria) => ({
-    type,
-    parameters,
-    searchCriteria
-  });
+  type,
+  parameters,
+  searchCriteria
+});
   
-  // TODO: Placeholder for handling direct download export requests
-  export const handleDirectDownloadRequest = async searchCriteria => {};
-  
-  export const handleEmailRequest = async (searchCriteria, emailAddress) => {
-    const request = getDownloadRequest("email", { emailAddress }, searchCriteria);
-    const response = await fetch(`${process.env.REACT_APP_API_URL || ""}/download-request/`,
-      {
-        method: "POST",
-        mode: "cors",
-        body: JSON.stringify(request),
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-  
-    if (!response.ok) throw Error(response.statusText);
+// TODO: Placeholder for handling direct download export requests
+const handleDirectDownloadRequest = async searchCriteria => {};
+
+const handleEmailRequest = async (searchCriteria, emailAddress) => {
+  const request = getDownloadRequest("email", { emailAddress }, searchCriteria);
+  const response = await fetch(`${process.env.REACT_APP_API_URL || ""}/download-request/`,
+    {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(request),
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+
+  if (!response.ok) throw Error(response.statusText);
+};
+
+// TODO: Placeholder for handling push notification export requests
+const handlePushNotificationRequest = async searchCriteria => {};
+
+export const handleModalSubmit = (modalData, lastRequest) => {
+  switch (modalData.selectedType) {
+    case "directDownload": 
+      handleDirectDownloadRequest();  
+      break;
+    case "email":
+      handleEmailRequest(lastRequest, modalData.emailAddress)
+      .then(() =>
+          toast.success("Success! You will shortly receive an email."))
+      .catch(error => {
+          toast.error("Something went wrong, please try again.");
+          console.error(error);
+        });
+      break;
+    case "pushNotification":
+      handlePushNotificationRequest();
+      break;
+    default:
+      break;
   };
-  
-  // TODO: Placeholder for handling push notification export requests
-  export const handlePushNotificationRequest = async searchCriteria => {};
-  
+};
