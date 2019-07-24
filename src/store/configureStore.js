42963be644995a96a6ea430c3d3f139c95967686
OliverForeman <oliverforeman@btinternet.com>
2019-07-24 10:56:11 +0100

    SDE-76 Added thunk middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
+import thunk from 'redux-thunk';
 
-export default initialState => createStore(rootReducer, initialState);
+const middleware = [thunk];
+
+export default initialState => createStore(rootReducer, initialState, applyMiddleware(...middleware));
