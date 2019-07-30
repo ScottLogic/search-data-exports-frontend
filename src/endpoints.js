844d6af6bc515fcc5f5005bf6ce788af7c9e63fe
OliverForeman <oliverforeman@btinternet.com>
2019-07-30 14:12:51 +0100

    SDE-104 Added report status endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,3 +3,5 @@ const rootURL = `${process.env.REACT_APP_API_URL || ''}`;
 export const SEARCH_REQUEST_URL = `${rootURL}/search/`;
 
 export const DOWNLOAD_REQUEST_URL = `${rootURL}/download-request`;
+
+export const REPORT_STATUS_URL = `${rootURL}/report-status`;
