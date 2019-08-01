4e995c284cab9ee5590e185eb4b49a9ba15e0eb4
OliverForeman <oliverforeman@btinternet.com>
2019-08-01 14:08:16 +0100

    SDE-107 Added websocket endpoint to endpoints file

--- src/endpoints.js
+++ src/endpoints.js
@@ -5,3 +5,5 @@ export const SEARCH_REQUEST_URL = `${rootURL}/search/`;
 export const DOWNLOAD_REQUEST_URL = `${rootURL}/download-request`;
 
 export const REPORT_STATUS_URL = `${rootURL}/report-status`;
+
+export const WEBSOCKET_ENDPOINT = 'wss://q05lp0yuc6.execute-api.eu-west-1.amazonaws.com/dev';
