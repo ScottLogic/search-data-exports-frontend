757fa776c50ece531e33c2ff80fe0df9d7e9bc47
OliverForeman <oliverforeman@btinternet.com>
2019-08-06 16:39:42 +0100

    SDE-131 Added tests for ReportsModal component

---
+++ src/components/reports/ReportsModal.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ReportsModal from './ReportsModal';
+import reportResultsRequest from '../../api/reportResults';
+
+Enzyme.configure({ adapter: new Adapter() });
+jest.mock('../../api/reportResults', () => jest.fn());
+
+describe('<ReportsModal />', () => {
+  let wrapper;
+
+  const defaultProps = {
+    showModal: true,
+    closeModal: jest.fn()
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<ReportsModal {...defaultProps} />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Clicking download for the trending report calls the api with the correct parameters', () => {
+    wrapper.find('input[value="Download"]').at(1).simulate('click');
+    expect(reportResultsRequest).toHaveBeenCalledTimes(1);
+    expect(reportResultsRequest).toHaveBeenCalledWith({
+      selectedType: 'pdf',
+      reportName: 'Trending'
+    });
+  });
+
+  it('Clicking download in the download modal calls the api with the correct parameters', () => {
+    wrapper.children().at(1).props().onSubmit({
+      preventDefault: jest.fn(),
+      target: {
+        selectedOption: {
+          value: 'test'
+        }
+      }
+    });
+
+    expect(reportResultsRequest).toHaveBeenCalledTimes(1);
+    expect(reportResultsRequest).toHaveBeenCalledWith({
+      selectedType: 'test',
+      reportName: ''
+    });
+  });
+});
