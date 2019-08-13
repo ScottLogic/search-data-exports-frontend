3ebd6f6d129835f1c462f555ede2f50427b32bb3
Scott Woods <swoods@scottlogic.com>
2019-08-13 16:18:33 +0100

    sde-67 initial delete request

--- src/api/digestDelete.js
+++ src/api/digestDelete.js
@@ -1,5 +1,31 @@
-// Placeholder for making the request to un-subscribe to a digest
-// An email address will also be required here
+import { API } from 'aws-amplify';
+import { toast } from 'react-toastify';
+import { DIGEST_SUBSCRIPTIONS } from '../endpoints';
+
+const deleteDailyDigest = ( request ) => {
+  console.log(request);
+  API.delete('APIGateway', DIGEST_SUBSCRIPTIONS, {
+    body: request
+  })
+    .then(() => toast.success('Delete Subscription successful'))
+    .catch((error) => {
+      const errorMessage = (error.response && error.response.data && error.response.data.message) || '';
+      if (errorMessage.length > 0) {
+        toast.error(errorMessage);
+      } else {
+        console.error(error);
+        toast.error(`Something went wrong, ${error}.`);
+      }
+    });
+};
+
 export default (request) => {
   console.log('Delete Digest Request:', request);
+  switch (request.freqency) {
+    case 'Daily':
+      deleteDailyDigest(request) 
+      break;
+    default:
+      console.warn(`Unknown method passed to delete ${request.frequency}`);
+  }
 };
