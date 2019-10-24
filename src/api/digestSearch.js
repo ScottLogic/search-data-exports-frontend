020d2d6d0bb302ea1bf7e2b95752523beb5baffa
Antreas Paschalis <apaschalis@scottlogic.com>
2019-10-24 12:48:28 +0100

    digestSearch API unit tests

--- src/api/digestSearch.js
+++ src/api/digestSearch.js
@@ -3,15 +3,13 @@ import { toast } from 'react-toastify';
 import { DIGEST_SUBSCRIPTIONS, REALTIME_SUBSCRIPTIONS } from '../endpoints';
 import { updateDigestList, updateRealTimeList } from '../actions/DigestList';
 
-const getSubscriptions = async (dispatch, endpoint, updateFunc) => {
-  API.get('APIGateway', endpoint, {})
-    .then(response => dispatch(updateFunc(response.map(value => ({ value })))))
-    .catch((error) => {
-      toast.error(`Error in Daily Digest List API. ${error}`);
-    });
-};
+const getSubscriptions = async (dispatch, endpoint, updateAction) => API.get('APIGateway', endpoint, {})
+  .then(response => dispatch(updateAction(response.map(value => ({ value })))))
+  .catch((error) => {
+    toast.error(`Error in Daily Digest List API: ${error}`);
+  });
 
-export default () => (dispatch) => {
+export default () => async (dispatch) => {
   getSubscriptions(dispatch, REALTIME_SUBSCRIPTIONS, updateRealTimeList);
   getSubscriptions(dispatch, DIGEST_SUBSCRIPTIONS, updateDigestList);
 };
