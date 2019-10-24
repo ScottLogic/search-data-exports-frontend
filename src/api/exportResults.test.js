d999906741cd0bc408baab0488ad0040f91f0b90
Antreas Paschalis <apaschalis@scottlogic.com>
2019-10-24 16:54:54 +0100

    Removed comments

--- src/api/exportResults.test.js
+++ src/api/exportResults.test.js
@@ -98,32 +98,4 @@ describe('exportResults API', () => {
       expect(toast.error).toHaveBeenCalledWith('Something went wrong, please try again.');
     });
   });
-
-
-  // it('makes API calls for both real time and daily subscriptions', async () => {
-  //   API.get.mockResolvedValue(['post1', 'post2', 'post3']);
-  //   digestSearch()(jest.fn());
-  //   expect(API.get).toHaveBeenCalledWith('APIGateway', REALTIME_SUBSCRIPTIONS, {});
-  //   expect(API.get).toHaveBeenCalledWith('APIGateway', DIGEST_SUBSCRIPTIONS, {});
-  // });
-
-  // it('dispatches the update action function with the retrieved subscriptions when the request is successful', async () => {
-  //   const mockDispatch = jest.fn();
-  //   const response = ['post1', 'post2', 'post3'];
-  //   const expectedActionPayload = response.map(value => ({ value }));
-  //   API.get.mockResolvedValue(['post1', 'post2', 'post3']);
-  //   await digestSearch()(mockDispatch);
-  //   expect(mockDispatch).toHaveBeenCalledTimes(2);
-  //   expect(mockDispatch).toHaveBeenCalledWith(updateRealTimeList(expectedActionPayload));
-  //   expect(mockDispatch).toHaveBeenCalledWith(updateDigestList(expectedActionPayload));
-  // });
-
-  // it('displays an error toast when a request fails', async () => {
-  //   const mockDispatch = jest.fn();
-  //   const message = 'Something went terribly wrong';
-  //   API.get.mockRejectedValue(message);
-  //   await digestSearch()(mockDispatch);
-  //   expect(toast.error).toHaveBeenCalledTimes(2);
-  //   expect(toast.error).toHaveBeenCalledWith('Error in Daily Digest List API: Something went terribly wrong');
-  // });
 });
