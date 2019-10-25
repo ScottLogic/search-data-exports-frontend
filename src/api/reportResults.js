69c7a4d5e32cc5271c8130e9a132eb1b3f7bd903
Antreas Paschalis <apaschalis@scottlogic.com>
2019-10-25 10:04:47 +0100

    removed unnecessary searchCriteria in reportResults API - YAGNI!

--- src/api/reportResults.js
+++ src/api/reportResults.js
@@ -2,40 +2,29 @@ import { toast } from 'react-toastify';
 import { API } from 'aws-amplify';
 import { GRAPHICAL_REQUEST, HYBRID_REQUEST } from '../endpoints';
 
-const emptySearch = {
-  search: []
-};
-
-const handleDownloadRequest = async (reportURL, searchCriteria, callback) => {
-  toast.info(`Request sent, ${(callback) ? 'your image will display shortly.' : 'your download will begin soon.'}`);
-
-  API.post('APIGateway', reportURL, {
-    body: {
-      ...searchCriteria,
-      download: (!callback)
+const handleDownloadRequest = async (reportURL, callback) => {
+  try {
+    const downloadLink = await API.post('APIGateway', reportURL, { body: { download: !callback } });
+    toast.info(`Request sent, ${(callback) ? 'your image will display shortly.' : 'your download will begin soon.'}`);
+    if (callback) {
+      callback(downloadLink);
+    } else {
+      window.location.assign(downloadLink);
     }
-  })
-    .then((downloadLink) => {
-      if (callback) {
-        callback(downloadLink);
-      } else {
-        window.location.assign(downloadLink);
-      }
-    })
-    .catch(() => {
-      toast.error('Something went wrong, please try again.');
-    });
+  } catch {
+    toast.error('Something went wrong, please try again.');
+  }
 };
 
-export default (modalData, callback) => {
+export default async (modalData, callback) => {
   switch (modalData.reportName) {
     case 'PostFreq':
-      handleDownloadRequest(GRAPHICAL_REQUEST, emptySearch, callback);
+      await handleDownloadRequest(GRAPHICAL_REQUEST, callback);
       break;
     case 'Trending':
-      handleDownloadRequest(HYBRID_REQUEST, emptySearch, callback);
+      await handleDownloadRequest(HYBRID_REQUEST, callback);
       break;
     default:
-      break;
+      throw Error(`Unknown report type selected: ${modalData.reportName}`);
   }
 };
